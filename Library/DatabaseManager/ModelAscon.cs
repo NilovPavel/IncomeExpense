286f920af25f6204417aad6b7c16cfbaaf587dae
NilovPavel <nilowpavel@gmail.com>
2021-06-20 14:58:33 +0300

    Changed name of model(intersect with name of a class).

---
+++ Library/DatabaseManager/ModelAscon.cs
@@ -0,0 +1,38 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DatabaseManager
+{
+    public partial class ModelAscon : DbContext
+    {
+        public ModelAscon()
+            : base("name=ModelAscon")
+        {
+        }
+
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<Data> Data { get; set; }
+        public virtual DbSet<Recepients> Recepients { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>()
+                .HasMany(e => e.Data)
+                .WithRequired(e => e.Categories)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Recepients>()
+                .HasMany(e => e.Data)
+                .WithRequired(e => e.Recepients)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Users>()
+                .HasMany(e => e.Data)
+                .WithRequired(e => e.Users)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
