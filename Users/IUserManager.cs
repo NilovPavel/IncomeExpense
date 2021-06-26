5fa5db44fbf1b07ed415c78fa30a82480d215307
NilovPavel <nilowpavel@gmail.com>
2021-06-26 16:22:18 +0300

    Added UserManager-packet.

---
+++ Users/IUserManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Users
+{
+    public interface IUserManager
+    {
+        string GetCurrentUserName();
+        string[] GetUserNames();
+    }
+}
