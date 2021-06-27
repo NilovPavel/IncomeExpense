453babc684620f6cdc32b493c718888331d57982
NilovPavel <nilowpavel@gmail.com>
2021-06-27 17:37:55 +0300

    Added behavior UserManager.

--- WindowsForms/Administrator.cs
+++ WindowsForms/Administrator.cs
@@ -24,6 +24,13 @@ namespace WindowsForms
             this.dataManager = dataManager;
             this.userManager = userManager;
             InitializeComponent();
+            this.Initialization();
+        }
+
+        private void Initialization()
+        {
+            this.valueOfMode.Text = "Администратор";
+            this.valueOfUsername.Text = this.userManager.GetCurrentUserName();
         }
 
         private void Exit_Click(object sender, EventArgs e)
@@ -33,9 +40,17 @@ namespace WindowsForms
 
         private void SelectUserButton_Click(object sender, EventArgs e)
         {
-            this.userManager = new MashineUserManager();
-            NewUser newUser = new NewUser(userManager);
+            NewUser newUser = new NewUser(dataManager, userManager);
             newUser.ShowDialog();
+            string selectedUser = newUser.SelectUser;
+            int inputCash = newUser.StartCash;
+
+            bool userExist = this.dataManager.GetUsers().FirstOrDefault(item => item.UserName.Equals(selectedUser)) != null;
+
+            if(!userExist && !string.IsNullOrEmpty(selectedUser))
+                this.dataManager.AddUser(newUser.SelectUser, newUser.StartCash);
+            
+            this.table.DataSource = this.dataManager.GetUsers();
         }
 
         private void UserManager_Click(object sender, EventArgs e)
