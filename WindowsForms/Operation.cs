c93d9f559da9855e42d52e59c0a8ec9f78f79ca4
NilovPavel <nilowpavel@gmail.com>
2021-07-04 16:59:24 +0300

    Warning about cash state sub zero.

--- WindowsForms/Operation.cs
+++ WindowsForms/Operation.cs
@@ -41,6 +41,12 @@ namespace WindowsForms
             DatabaseManager.Users user = this.dataManager.GetUsers().FirstOrDefault(item => item.UserName.Equals(this.userManager.GetCurrentUserName()));
 
             int amount = int.Parse(this.summTextBox.Text) * (this.isEnrollmentCheckBox.Checked ? 1 : -1);
+
+            int summaryCash = this.dataManager.GetData().Where(data => data.userId == user.userId).Sum(item => item.CashChange);
+            
+            if (user.StartCash + summaryCash < 0) 
+                MessageBox.Show("Состояние вашего счета после операции станет меньше нуля!");
+
             this.dataManager.AddUserOperation(user.userId, category.categoryId, recepients.recepientId, amount);
         }
     }
